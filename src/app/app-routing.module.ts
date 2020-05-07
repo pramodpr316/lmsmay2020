b03e9ff1b0b0ed34a10982a02502f4c1c164fa84
gj1153 <pramodpr316@users.noreply.github.com>
2020-05-07 19:23:00 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LayoutComponent } from "./layout/layout.component";
+import { AdminComponent } from "./admin/admin.component";
+import { BorrowerComponent } from "./borrower/borrower.component";
+import { LibrarianComponent } from "./librarian/librarian.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: "lms/home",
+        pathMatch: "full",
+      },
+      {
+        path: "lms/home",
+        component: HomeComponent,
+      },
+      {
+        path: "lms/admin",
+        component: AdminComponent,
+      },
+      {
+        path: "lms/borrower",
+        component: BorrowerComponent,
+      },
+      {
+        path: "lms/librarian",
+        component: LibrarianComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AdminComponent } from './admin/admin.component';
+import { LibrarianComponent } from './librarian/librarian.component';
+import { BorrowerComponent } from './borrower/borrower.component';
+import { AuthorComponent } from './admin/author/author.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, LayoutComponent, HeaderComponent, FooterComponent, AdminComponent, LibrarianComponent, BorrowerComponent, AuthorComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
